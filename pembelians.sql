f7981b235e77a1ce503ffe0cab246421c0691ffc
Kazhim <94035733+Kazhim@users.noreply.github.com>
2022-07-06 21:47:53 +0700

    Add files via upload

---
+++ pembelians.sql
@@ -0,0 +1,24 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.33 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping data for table db_rest-api.pembelians: ~0 rows (approximately)
+/*!40000 ALTER TABLE `pembelians` DISABLE KEYS */;
+INSERT INTO `pembelians` (`id`, `ayam`, `ayam_goreng`, `ayam_panggang`, `total_harga`, `created_at`, `updated_at`) VALUES
+	(1, 10000, 50000, 54000, 60000, NULL, '2022-07-06 14:36:24');
+/*!40000 ALTER TABLE `pembelians` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
